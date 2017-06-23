2a28f527de7ad6637b0447f3fb58daa28f15096c
zeroabe <mikhail.musofranov@tmc-consulting.ru>
2017-06-23 17:28:52 +0300

    Объект ответа на запрос открытия смены теперь тоже реализует соответствующий интерфейс

--- src/Models/Responses/OpenShift.php
+++ src/Models/Responses/OpenShift.php
@@ -7,8 +7,9 @@ use JMS\Serializer\Annotation\Accessor;
 use JMS\Serializer\Annotation\AccessType;
 use JMS\Serializer\Annotation\Type;
 use KKMClient\Interfaces\ResponseInterface;
+use KKMClient\Traits\CommonResponseTrait;
 
 class OpenShift implements ResponseInterface
 {
-
+    use CommonResponseTrait;
 }
